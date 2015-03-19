4de7ffe2e1c076e5884dda0ed1f41c029e1c3ebf
Nguyễn Đình Quân <dinhquan@narga.net>
2015-03-19 15:37:49 +0700

    Redesign Footer

--- footer.php
+++ footer.php
@@ -11,23 +11,28 @@
 ?>
 
 <!-- End row for main content area -->
-    </section>
-
-    <!-- Footer area -->
-    <footer role="contentinfo" class="row">
-    <div id="footer-widgets" class="footer-info hide-for-small">
+<aside class="columns" id="sub-footer">
     <?php dynamic_sidebar("footer-1"); ?>
-    <div class="colophon">
-    <hr />
-        <div class="large-6 medium-6 columns">
-        <?php printf( __('<p>Proudly powered by <a href="http://wordpress.org/" title="%1$s" rel="generator">%2$s</a> &middot; <a href="http://www.narga.net/" title="%3$s" rel="designer">%4$s</a>.</p>', 'narga'), esc_attr( 'A Semantic Personal Publishing Platform'), 'WordPress', esc_attr( 'NARGA'), 'NARGA'); ?>
+</aside>
+</section>
+
+<!-- Footer area -->
+<footer role="contentinfo">
+    <div id="colophon" class="row">
+        <div id="footer-info" class="left">
+        <?php
+        if (narga_options('footer_info') != '')
+            echo narga_options('footer_info');
+        else
+            printf( __('<p>Proudly powered by <a href="http://wordpress.org/" title="%1$s" rel="generator">%2$s</a> &middot; <a href="http://www.narga.net/" title="%3$s" rel="designer">%4$s</a>.</p>', 'narga'), esc_attr( 'A Semantic Personal Publishing Platform'), 'WordPress', esc_attr( 'NARGA'), 'NARGA');
+        ?>
         </div>
-        <div class="large-6 medium-6 columns hide-for-medium">
+        <div class="right show-for-medium-up">
         <?php narga_footer_navigation(); ?>
         </div>
     </div>
-    </div>
-    </footer>
+
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
